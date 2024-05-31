10f114fc25e74b5c958a9071710d3f74a46932c2
Filip Holik <filipholik@users.noreply.github.com>
2024-05-31 11:07:49 +0100

    Final version

--- Scripts/scapy_asset_discovery.py
+++ Scripts/scapy_asset_discovery.py
@@ -5,20 +5,28 @@ from datetime import datetime
 GOOSE_PROTOCOL = 0x88B8
 INTERFACE = "DPSHMI-eth0"
 
-packet_count = 0 
-bytes_count = 0 
+mac_packet_count = {}
+mac_bytes_count = {}
 
 def packet_callback(packet): 
      global packet_count
      global bytes_count
 
      if Ether in packet and packet[Ether].type == GOOSE_PROTOCOL:
-          packet_count += 1
+          src_mac = packet[Ether].src 
+          if src_mac not in mac_packet_count: 
+               mac_packet_count[src_mac] = 0 
+          mac_packet_count[src_mac] += 1 
+
+          if src_mac not in mac_bytes_count: 
+               mac_bytes_count[src_mac] = 0 
+          mac_bytes_count[src_mac] += len(packet) 
 
           timestamp_capture = packet.time
           timestamp_ns = time.time_ns()
           current_time = datetime.fromtimestamp(timestamp_ns / 1e9)
           current_time_str = current_time.strftime("%Y-%m-%d %H:%M:%S") + f'.{timestamp_ns % 1_000_000_000:09d}'
           print(f"New GOOSE received at {current_time_str}: {packet.summary()}")
+          print(f"Total packets: " + str(mac_packet_count[src_mac]) + ", bytes: " + str(mac_bytes_count[src_mac]))
 
 sniff(prn=packet_callback, iface=INTERFACE, filter="ether proto 0x88B8", store=0)
