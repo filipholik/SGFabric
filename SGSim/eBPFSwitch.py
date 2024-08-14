a21ecedae0b7aee80e163401cef41acce2c043df
Filip Holik <filipholik@users.noreply.github.com>
2024-08-14 15:17:29 +0100

    Create eBPFSwitch.py

---
+++ SGSim/eBPFSwitch.py
@@ -0,0 +1,49 @@
+from mininet.node import Switch, Host
+import subprocess
+
+class eBPFHost(Host):
+    def config(self, **params):
+        r = super(Host, self).config(**params)
+
+        # Disable offloading
+        for off in ["rx", "tx", "sg"]:
+            cmd = "/sbin/ethtool --offload {} {} off".format(self.defaultIntf(), off)
+            self.cmd(cmd)
+
+        return r
+
+class eBPFSwitch(Switch):
+    dpid = 1
+
+    def __init__(self, name, switch_path='softswitch', dpid=None, **kwargs):
+        Switch.__init__(self, name, str(dpid), **kwargs) #F: Fixed missing DPID argument pass to Mininet 
+
+        self.switch_path = switch_path
+
+        if dpid:
+            self.dpid = dpid
+            eBPFSwitch.dpid = max(eBPFSwitch.dpid, dpid)
+        else:
+            self.dpid = eBPFSwitch.dpid
+            eBPFSwitch.dpid += 1
+
+    @classmethod
+    def setup(cls):
+        pass
+
+    def start(self, controllers):
+        print("Starting eBPF switch", self.name)
+
+        args = [self.switch_path]
+
+        args.extend(['-p', '-i', '--dpid', str(self.dpid)])
+
+        for port, intf in self.intfs.items():
+            if not intf.IP():
+                args.append(intf.name)
+
+        self.proc = subprocess.Popen(args)
+
+    def stop(self):
+        print('stopping')
+        self.proc.kill()
