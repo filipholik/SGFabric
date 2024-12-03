0115b9e247a87ad5950557d2e45b36e861485495
Filip Holik <filipholik@users.noreply.github.com>
2024-12-03 16:02:23 +0000

    Scripts added, BW settings

--- Scripts/Orchestration/ThreeTier.py
+++ Scripts/Orchestration/ThreeTier.py
@@ -49,42 +49,44 @@ def smartGridSimNetwork():
     A4 = net.addSwitch('A4', switch_path=switchPath, dpid=10) 
 
     info( '*** Starting hosts \n')
-    H1 = net.addHost('DSS1RTU', cls=eBPFHost, ip='10.0.0.1', defaultRoute='10.10.10.10',mac='00:00:00:00:00:01')
-    H2 = net.addHost('DSS2RTU', cls=eBPFHost, ip='10.0.0.2', defaultRoute='10.10.10.10',mac='00:00:00:00:00:02')
-    H3 = net.addHost('CONTROL', cls=eBPFHost, ip='10.0.0.3', defaultRoute='10.10.10.10',mac='00:00:00:00:00:03')
-    H4 = net.addHost('IED1', cls=eBPFHost, ip='10.0.0.4', defaultRoute='10.10.10.10',mac='00:00:00:00:00:04')
-    H5 = net.addHost('IED2', cls=eBPFHost, ip='10.0.0.5', defaultRoute='10.10.10.10',mac='00:00:00:00:00:05')
-    H6 = net.addHost('IED3', cls=eBPFHost, ip='10.0.0.6', defaultRoute='10.10.10.10',mac='00:00:00:00:00:06')
-    H7 = net.addHost('IED4', cls=eBPFHost, ip='10.0.0.7', defaultRoute='10.10.10.10',mac='00:00:00:00:00:07')
-    H8 = net.addHost('DPSHMI', cls=eBPFHost, ip='10.0.0.8', defaultRoute='10.10.10.10',mac='00:00:00:00:00:08')
+    H1 = net.addHost('H1', cls=eBPFHost, ip='10.0.0.1', defaultRoute='10.10.10.10',mac='00:00:00:00:00:01')
+    H2 = net.addHost('H2', cls=eBPFHost, ip='10.0.0.2', defaultRoute='10.10.10.10',mac='00:00:00:00:00:02')
+    H3 = net.addHost('H3', cls=eBPFHost, ip='10.0.0.3', defaultRoute='10.10.10.10',mac='00:00:00:00:00:03')
+    H4 = net.addHost('H4', cls=eBPFHost, ip='10.0.0.4', defaultRoute='10.10.10.10',mac='00:00:00:00:00:04')
+    H5 = net.addHost('H5', cls=eBPFHost, ip='10.0.0.5', defaultRoute='10.10.10.10',mac='00:00:00:00:00:05')
+    H6 = net.addHost('H6', cls=eBPFHost, ip='10.0.0.6', defaultRoute='10.10.10.10',mac='00:00:00:00:00:06')
+    H7 = net.addHost('H7', cls=eBPFHost, ip='10.0.0.7', defaultRoute='10.10.10.10',mac='00:00:00:00:00:07')
+    H8 = net.addHost('H8', cls=eBPFHost, ip='10.0.0.8', defaultRoute='10.10.10.10',mac='00:00:00:00:00:08')
     SERVER = net.addHost('SERVER', cls=eBPFHost, ip='10.10.10.10', defaultRoute='10.10.10.10',mac='00:00:00:00:00:10')
 
     info( '*** Setting link parameters\n')
     #WAN1 = {'bw':1000,'delay':'20ms','loss':1,'jitter':'10ms'} 
     #GBPS = {'delay':'18ms'} 
     MBPS = {'bw':1} 
+    MBPS10 = {'bw':10}
+    MBPS100 = {'bw':100}
 
     info( '*** Adding links\n')
     net.addLink(H1, A1, cls=TCLink , **MBPS)
     net.addLink(H2, A1, cls=TCLink , **MBPS)
     net.addLink(H3, A2, cls=TCLink , **MBPS)
     net.addLink(H4, A2, cls=TCLink , **MBPS)
-    net.addLink(A1, D1, cls=TCLink , **MBPS)
-    net.addLink(A2, D2, cls=TCLink , **MBPS)
-    net.addLink(D1, C1, cls=TCLink , **MBPS)
-    net.addLink(D2, C1, cls=TCLink , **MBPS)
+    net.addLink(A1, D1, cls=TCLink , **MBPS10)
+    net.addLink(A2, D2, cls=TCLink , **MBPS10)
+    net.addLink(D1, C1, cls=TCLink , **MBPS10)
+    net.addLink(D2, C1, cls=TCLink , **MBPS10)
     
     net.addLink(H5, A3, cls=TCLink , **MBPS)
     net.addLink(H6, A3, cls=TCLink , **MBPS)
     net.addLink(H7, A4, cls=TCLink , **MBPS)
     net.addLink(H8, A4, cls=TCLink , **MBPS)
-    net.addLink(A3, D3, cls=TCLink , **MBPS)
-    net.addLink(A4, D4, cls=TCLink , **MBPS)
-    net.addLink(D3, C2, cls=TCLink , **MBPS)
-    net.addLink(D4, C2, cls=TCLink , **MBPS)
+    net.addLink(A3, D3, cls=TCLink , **MBPS10)
+    net.addLink(A4, D4, cls=TCLink , **MBPS10)
+    net.addLink(D3, C2, cls=TCLink , **MBPS10)
+    net.addLink(D4, C2, cls=TCLink , **MBPS10)
 
-    net.addLink(C1, C2, cls=TCLink , **MBPS)
-    net.addLink(D4, SERVER, cls=TCLink , **MBPS)
+    net.addLink(C1, C2, cls=TCLink , **MBPS100)
+    net.addLink(D4, SERVER, cls=TCLink , **MBPS10)
 
     info( '*** Starting network\n')
     net.build()
@@ -105,17 +107,36 @@ def smartGridSimNetwork():
     net.get('A4').start([])
 
     info( '*** Preparing custom scripts \n')
-    CLI.do_attack_dos = attack_dos
+    CLI.do_orch_measure = orch_measure
+    CLI.do_orch_attack_ddos = orch_attack_ddos
+    CLI.do_orch_attack_dos = orch_attack_dos
     info( '*** Three Tier Topology Started *** \n' )
     CLI(net)
 
     net.stop()
 
-def attack_dos(self, line):
+def orch_attack_ddos(self, line):
+    "Starts the DDoS attack to the server." 
+    net = self.mn  
+    info('Starting DDoS attack... \n')    
+    net.get('H1').cmdPrint('xterm -geometry 90x30+10+10 -fa "Monospace" -fs 12 -T "H1-DDOS" -e "sudo hping3 -S --flood 10.10.10.10;bash"&') 
+    net.get('H2').cmdPrint('xterm -geometry 90x30+10+10 -fa "Monospace" -fs 12 -T "H2-DDOS" -e "sudo hping3 -S --flood 10.10.10.10;bash"&') 
+    net.get('H4').cmdPrint('xterm -geometry 90x30+10+10 -fa "Monospace" -fs 12 -T "H4-DDOS" -e "sudo hping3 -S --flood 10.10.10.10;bash"&') 
+    net.get('H6').cmdPrint('xterm -geometry 90x30+10+10 -fa "Monospace" -fs 12 -T "H6-DDOS" -e "sudo hping3 -S --flood 10.10.10.10;bash"&') 
+
+def orch_attack_dos(self, line):
     "Starts the DoS attack to the server." 
     net = self.mn  
     info('Starting DoS attack... \n')    
-    net.get('H1').cmdPrint('xterm -geometry 90x30+10+10 -fa "Monospace" -fs 12 -T "ATTACKER-DOS" -e "sudo hping3 -S --flood 10.10.10.10;bash"&') 
+    net.get('H1').cmdPrint('xterm -geometry 90x30+10+10 -fa "Monospace" -fs 12 -T "H1-DDOS" -e "sudo hping3 -S --flood 10.10.10.10;bash"&') 
+
+def orch_measure(self, line):
+    "Starts throughput measurement." 
+    net = self.mn  
+    info('Starting throughput measurement... \n')    
+    net.get('SERVER').cmdPrint('xterm -geometry 90x30+10+10 -fa "Monospace" -fs 12 -T "SERVER-Service" -e "iperf -s;bash"&') 
+    time.sleep(1)
+    net.get('H3').cmdPrint('xterm -geometry 90x30+10+10 -fa "Monospace" -fs 12 -T "H3-Client" -e "iperf -c 10.10.10.10;bash"&') 
 
 if __name__ == '__main__':
     setLogLevel( 'info' )
