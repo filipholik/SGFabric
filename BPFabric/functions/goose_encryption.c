1eba22301523fb4a9a7cacb5605846466c570bd1
Filip Holik <filipholik@users.noreply.github.com>
2025-03-27 11:40:13 +0000

    GOOSE Encryption Function

---
+++ BPFabric/functions/goose_encryption.c
@@ -0,0 +1,18 @@
+#include <linux/if_ether.h>
+#include "ebpf_switch.h"
+
+/*
+Function which sends GOOSE messages from physical ports to the encryption / decryption stack via a virtual port. 
+*/
+
+uint64_t prog(struct packet *pkt)
+{
+    if (pkt->metadata.in_port != 0 && pkt->eth.h_proto == 47240) //Physical ports
+    {
+        bpf_mirror(0, pkt, pkt->metadata.length); // Virtual port      
+        return DROP; 
+    }
+  
+    return NEXT;
+}
+char _license[] SEC("license") = "GPL";
