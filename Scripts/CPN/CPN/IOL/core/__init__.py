8b4fb6199339aa6bffb6ef4f50088f218833200e
Filip Holik <filipholik@users.noreply.github.com>
2025-10-20 10:48:43 +0100

    Cleanup

--- Scripts/CPN/CPN/IOL/core/__init__.py
+++
@@ -1,3 +0,0 @@
-from .application import eBPFCoreApplication
-from .events import set_event_handler
-from .protocol import FLOOD, CONTROLLER, DROP
--- Scripts/CPN/CPN/IOL/core/events.py
+++
@@ -1,7 +0,0 @@
-_handlers = {}
-
-def set_event_handler(opcode):
-    def set_event_handler_decorator(func):
-        _handlers.setdefault(opcode, []).append(func)
-        return func
-    return set_event_handler_decorator
